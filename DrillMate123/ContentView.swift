d07012207687b1c86616dbaba8f01396940943e3
Mahesh Behere <maheshbehere14@gmail.com>
2025-08-26 12:20:52 +0530

    initial commit for project setup and .gitignore added

---
+++ DrillMate123/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrillMate123
+//
+//  Created by Mahesh Behere on 26/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrillMate123/DrillMate123App.swift
@@ -0,0 +1,17 @@
+//
+//  DrillMate123App.swift
+//  DrillMate123
+//
+//  Created by Mahesh Behere on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DrillMate123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
